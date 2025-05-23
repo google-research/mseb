da23de51710be924b1a5856e35f4f246ca7a2a35
The MSEB Authors <no-reply@google.com>
2025-05-23 08:19:14 -0700

    Adds scripts for computing IDF tables from Wikipedia.
    PiperOrigin-RevId: 762430727

---
+++ mseb/scripts/compute_idf_tables.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+output_dir="${HOME}/tmp/idf_tables"
+python_bindir="${HOME}/python/bin"
+scripts_dir=`pwd`
+
+languages="ar bn en fi gu hi id ja kn ko ml mr ru sw ta te ur"
+cpus=30
+
+# We use the spacy multi-lamguage tokenizer for these languages:
+#  - 'sw': no spacy support,
+#  - 'ko': encountered out-of-bound errors when using 'ko' tokenizer.
+function set_spacy_language() {
+  case $language in
+    "ko"|"sw")
+      spacy_language="xx"
+      ;;
+    *)
+      spacy_language=${language}
+  esac
+}
+
+# Some REs in wikiextractor do not follow the lastest format.
+# Apply fix. This should be safe.
+function patch_wikiextractor() {
+  local location=`~/python/bin/pip show -f wikiextractor | grep "Location: "`
+  local file=${location#"Location: "}/wikiextractor/extract.py
+  local backup=${file}.bak
+  if [ ! -s ${backup} ]; then
+    cp -a ${file} ${backup}
+  fi
+  echo "Patching ${file}..."
+  sed -e 's/\\\[(((?i)/(?i)\\\[((/' ${backup} |
+  sed -e 's#r"""\^(http://|https://)#r"""(?i)^(http://|https://)#' |
+  sed -e 's#((?i)gif|png|jpg|jpeg)#(gif|png|jpg|jpeg)#' > ${file}
+  diff ${backup} ${file}
+}
+
+# Warning: there is a memory leak in the spacy Japanese tokenizer.
+# See https://github.com/explosion/spaCy/issues/13684
+# A workaround is to comment out line 89 of the following file in your
+# local spacy installation:
+#  spacy/lang/ja/__init__.py
+# The following function apply the workaround but it might break some features
+# spacy Japanese models so we'll revert that change at the end of the script.
+function patch_spacy() {
+  local location=`~/python/bin/pip show -f spacy | grep "Location: "`
+  local file=${location#"Location: "}/spacy/lang/ja/__init__.py
+  local backup=${file}.bak
+  if [ ! -s ${backup} ]; then
+    cp -a ${file} ${backup}
+  fi
+  echo "Patching ${file}..."
+  sed -e 's/token.morph = MorphAnalysis(self\.vocab, morph)/#token.morph = MorphAnalysis(self\.vocab, morph)/' ${backup} > ${file}
+  diff ${backup} ${file}
+  spacy_ja_file=${file}
+  spacy_ja_backup=${backup}
+}
+
+# Restore the spacy Japanese tokenizer to its original state.
+function restore_spacy() {
+  echo "Restoring ${spacy_ja_file}..."
+  cp -a ${spacy_ja_backup} ${spacy_ja_file}
+}
+
+${python_bindir}/pip install -U pip setuptools wheel
+${python_bindir}/pip install -U spacy[ja] unicodecsv wikiextractor
+
+patch_wikiextractor
+patch_spacy
+
+for language in ${languages}; do
+  wiki=${language}wiki
+  set_spacy_language
+  echo "${wiki}, using spacy language: ${spacy_language}"
+  mkdir -p ${output_dir}/${wiki}
+  pushd ${output_dir}/${wiki}
+  wget https://dumps.wikimedia.org/${wiki}/20250401/${wiki}-20250401-pages-articles.xml.bz2
+  mkdir -p data
+  ${python_bindir}/wikiextractor -o data --processes=${cpus} --json  ${wiki}-20250401-pages-articles.xml.bz2
+  ${python_bindir}/python ${scripts_dir}/wikipediaidf-spacy.py -s ${spacy_language} -i data/*/*  -o idf -c ${cpus}
+  popd
+done
+
+restore_spacy
---
+++ mseb/scripts/wikipediaidf-spacy.py
@@ -0,0 +1,157 @@
+# Copyright 2024 The MSEB Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Generate IDF table from Wikipedia articles."""
+
+# Original code:
+#   https://github.com/marcocor/wikipedia-idf
+#
+# Modified as follows:
+#  - Use spacy instead of nltk for word tokenization.
+#  - Remove stemming support.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import bz2
+import collections
+import contextlib
+import json
+import logging
+import math
+import multiprocessing
+import re
+import sys
+import spacy
+import unicodecsv as csv
+
+
+DROP_TOKEN_RE = re.compile("^\\W*$")
+model = None
+
+
+def filter_tokens(tokens):
+  for t in tokens:
+    if not DROP_TOKEN_RE.match(t):
+      yield t.lower()
+
+
+def get_file_reader(filename):
+  if filename.endswith(".bz2"):
+    return bz2.BZ2File(filename)
+  else:
+    return open(filename)
+
+
+def get_lines(input_files):
+  for filename in input_files:
+    with get_file_reader(filename) as f:
+      for line in f:
+        yield line
+
+
+def process_line(line):
+  article_json = json.loads(line)
+  return set(filter_tokens(tokenize_line(article_json["text"])))
+
+
+def tokenize_line(line):
+  tokens = []
+  sublines = line.split("\n")
+  with model.memory_zone():
+    for doc in model.pipe(sublines, disable=model.pipe_names):
+      for token in doc:
+        tokens.append(token.text)
+  return tokens
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      "-i",
+      "--input",
+      required=True,
+      nargs="+",
+      action="store",
+      help="Input JSON files",
+  )
+  parser.add_argument(
+      "-s",
+      "--spacy_lang",
+      required=True,
+      metavar="LANG",
+      help="IETF language tag, such as ‘en’, of the language.",
+  )
+  parser.add_argument(
+      "-o",
+      "--output",
+      metavar="OUT_BASE",
+      required=True,
+      help="Output CSV files base",
+  )
+  parser.add_argument(
+      "-l",
+      "--limit",
+      metavar="LIMIT",
+      type=int,
+      help="Stop after reading LIMIT articles.",
+  )
+  parser.add_argument(
+      "-c", "--cpus", default=1, type=int, help="Number of CPUs to employ."
+  )
+  args = parser.parse_args()
+
+  global model
+  model = spacy.blank(args.spacy_lang)
+  tokens_c = collections.Counter()
+  articles = 0
+  total_tokens = 0
+
+  with contextlib.closing(multiprocessing.Pool(processes=args.cpus)) as pool:
+    for tokens in pool.imap_unordered(process_line, get_lines(args.input)):
+      if not tokens:
+        continue
+      tokens_c.update(tokens)
+      total_tokens += len(tokens)
+      articles += 1
+      if not (articles % 100):
+        logging.info(
+            "Done %d articles; %d total tokens; %d unique tokens.",
+            articles,
+            total_tokens,
+            len(tokens_c),
+        )
+      if articles == args.limit:
+        break
+
+  with open("{}_{}".format(args.output, "terms.csv"), "wb") as o:
+    w = csv.writer(o, encoding="utf-8")
+    w.writerow(("token", "frequency", "total", "idf"))
+    for token, freq in tokens_c.most_common():
+      w.writerow([token, freq, articles, math.log(float(articles) / freq)])
+
+
+if __name__ == "__main__":
+  logging.basicConfig(level=logging.DEBUG)
+  sys.exit(main())
