ffcf08eff4cad093fd678df1b032b7a3f4463162
The MSEB Authors <no-reply@google.com>
2025-05-26 02:32:57 -0700

    Adding `RerankingEvaluator`
    This evaluator takes an audio sequence and a set of candidate texts (which can be associated with embeddings), encodes the audio sequence and reranks the candidate texts, and returns metrics on the reranking result.
    
    It uses a `BruteForceRetriever` internally.
    
    PiperOrigin-RevId: 763343744

---
+++ mseb/evaluators/reranking_evaluator.py
@@ -0,0 +1,132 @@
+# Copyright 2024 The MSEB Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Evaluator for retrieval tasks."""
+
+from __future__ import annotations
+
+from typing import Dict, List, Sequence, Union
+
+import jiwer
+from mseb import encoder
+from mseb import evaluator
+from mseb.evaluators import retrieval_evaluator
+from mseb.evaluators import retriever as retriever_lib
+import numpy as np
+from whisper.normalizers import basic
+from whisper.normalizers import english
+
+
+compute_reciprocal_rank = retrieval_evaluator.compute_reciprocal_rank
+
+
+def compute_word_errors(
+    truth: str, hypothesis: str, *, is_english: bool
+) -> tuple[float, float]:
+  """Computes the word error rate (WER)."""
+  text_transform = (
+      english.EnglishTextNormalizer()
+      if is_english
+      else basic.BasicTextNormalizer()
+  )
+
+  results = jiwer.compute_measures(
+      truth=[text_transform(truth)],
+      hypothesis=[text_transform(hypothesis)],
+  )
+  return (
+      results['substitutions'] + results['deletions'] + results['insertions'],
+      results['hits'] + results['substitutions'] + results['deletions'],
+  )
+
+
+def compute_correct(truth: str, hypothesis: str, *, is_english: bool) -> float:
+  """Computes the query error rate (QER)."""
+  text_transform = (
+      english.EnglishTextNormalizer()
+      if is_english
+      else basic.BasicTextNormalizer()
+  )
+
+  correct = float(text_transform(truth) != text_transform(hypothesis))
+  return correct
+
+
+class RerankingEvaluator(evaluator.Evaluator):
+  """Evaluator for retrieval tasks."""
+
+  def __call__(
+      self,
+      sequence: Union[str, Sequence[float]],
+      context: encoder.ContextParams,
+      candidate_texts: Sequence[str] = tuple(),
+      candidate_embeddings: np.ndarray = np.empty((0, 0)),
+      document_top_k: int = 10,
+  ) -> dict[str, float]:
+    """Evaluates quality of the encoder for input sequence and return metrics.
+
+    Args:
+      sequence: Input sound sequence to encode. String-type sequences are
+        interpreted as sound file paths.
+      context: Encoder input context parameters.
+      candidate_texts: Candidate texts to rank.
+      candidate_embeddings: Candidate embeddings to rank.
+      document_top_k: Number of documents to retrieve.
+
+    Returns:
+      Dictionary of metrics, including reciprocal rank, query error, and word
+      error.
+    """
+    _, query_embeddings = self.sound_encoder.encode(
+        sequence=sequence, context=context, **self.encode_kwargs
+    )
+    retriever = retriever_lib.ExactMatchRetriever()
+    retriever.build_index(
+        all_doc_embeds=candidate_embeddings, ids=candidate_texts
+    )
+    ranked_doc_ids, _ = retriever.retrieve_docs(
+        query_embeddings, document_top_k=document_top_k
+    )
+
+    assert context.language is not None
+    is_english = context.language.lower() == 'en'
+
+    word_errors, word_errors_weight = compute_word_errors(
+        truth=candidate_texts[0],
+        hypothesis=ranked_doc_ids[0][0],
+        is_english=is_english,
+    )
+
+    return {
+        'reciprocal_rank': compute_reciprocal_rank(
+            candidate_texts[0], ranked_doc_ids[0]
+        ),
+        'word_errors': word_errors,
+        'word_errors_weight': word_errors_weight,
+        'correct': compute_correct(
+            truth=candidate_texts[0],
+            hypothesis=ranked_doc_ids[0][0],
+            is_english=is_english,
+        ),
+    }
+
+  def combine_scores(self, scores: List[Dict[str, float]]) -> Dict[str, float]:
+    return evaluator.compute_weighted_average_and_std(
+        scores,
+        (
+            ('reciprocal_rank', 'mrr'),
+            ('word_errors', 'wer'),
+            ('correct', 'qer'),
+        ),
+    )
---
+++ mseb/evaluators/reranking_evaluator_test.py
@@ -0,0 +1,95 @@
+# Copyright 2024 The MSEB Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Sequence, Tuple, Union
+
+from absl.testing import absltest
+from mseb import encoder
+from mseb.evaluators import reranking_evaluator
+import numpy as np
+import numpy.testing as npt
+
+
+class IdentityEncoder(encoder.Encoder):
+
+  def encode(
+      self,
+      sequence: Union[str, Sequence[float]],
+      context: encoder.ContextParams,
+      index: int = 0,
+  ) -> Tuple[np.ndarray, np.ndarray]:
+    timestamps = np.array([[0.0, 1.0]])
+    embeddings = np.array([sequence], dtype=np.float32)
+    return timestamps, embeddings
+
+
+class RerankingEvaluatorTest(absltest.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.identity_encoder = IdentityEncoder()
+    self.context = encoder.ContextParams(language='en')
+
+  def test_call(self):
+    evaluator = reranking_evaluator.RerankingEvaluator(
+        sound_encoder=self.identity_encoder,
+        encode_kwargs={},
+    )
+    scores = evaluator(
+        np.array([2.5, 3.0]),
+        self.context,
+        candidate_texts=('b l i', 'b l a', 'x y z'),
+        candidate_embeddings=np.array([
+            [3.0, 4.0],
+            [5.0, 6.0],
+            [1.0, 2.0],
+        ]),
+        document_top_k=2,
+    )
+    npt.assert_equal(len(scores), 4)
+    npt.assert_equal(scores['reciprocal_rank'], 1 / 2)
+    npt.assert_equal(scores['word_errors'], 1)
+    npt.assert_equal(scores['word_errors_weight'], 3)
+    npt.assert_equal(scores['correct'], 1)
+
+  def test_combine_scores(self):
+    evaluator = reranking_evaluator.RerankingEvaluator(
+        sound_encoder=self.identity_encoder,
+        encode_kwargs={},
+    )
+    combined_scores = evaluator.combine_scores([
+        {
+            'reciprocal_rank': 1,
+            'correct': 1,
+            'word_errors': 2,
+            'word_errors_weight': 3,
+        },
+        {
+            'reciprocal_rank': 1 / 2,
+            'correct': 0,
+            'word_errors': 1,
+            'word_errors_weight': 2,
+        },
+    ])
+    npt.assert_equal(len(combined_scores), 6)
+    npt.assert_equal(combined_scores['mrr'], 3 / 4)
+    npt.assert_equal(combined_scores['mrr_std'], 1 / 4)
+    npt.assert_equal(combined_scores['qer'], 1 / 2)
+    npt.assert_equal(combined_scores['qer_std'], 1 / 2)
+    npt.assert_equal(combined_scores['wer'], 8 / 5)
+    npt.assert_equal(combined_scores['wer_std'], (6 / 25) ** 0.5)
+
+
+if __name__ == '__main__':
+  absltest.main()
