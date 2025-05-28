fc8fe38687c7c0a778ecc7d7a98a277e82ca9aab
Tom Bagby <tombagby@google.com>
2025-05-28 12:26:53 -0700

    Adding huggingface dataset definition.
    PiperOrigin-RevId: 764368885

--- mseb/svq_data.py
+++ mseb/svq_data.py
@@ -1,4 +1,4 @@
-# Copyright 2024 The MSEB Authors.
+# Copyright 2025 The MSEB Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,6 +17,7 @@
 import io
 import os
 from array_record.python import array_record_module as array_record
+import datasets
 import librosa
 import numpy as np
 import pandas as pd
@@ -110,3 +111,51 @@ def generate_examples(filepath, resample_hz: float | None = None):
     utt = utt_lookup(ex["utt_id"])
     ex["waveform"] = utt
     yield ex
+
+
+_CITATION = """\
+@InProceedings{mseb,
+title = {Massive Sound Embedding Benchmark (MSEB)},
+author={Georg Heigold, Ehsan Variani, Tom Bagby, Ji Ma, Cyril Allauzen, Shankar Kumar, Michael Riley}
+year={2025}
+}
+"""
+
+
+class SvqDataset(datasets.GeneratorBasedBuilder):
+  """SVQ dataset."""
+
+  VERSION = datasets.Version("1.1.0")
+
+  BUILDER_CONFIGS = [
+      datasets.BuilderConfig(
+          name="span_reasoning_in_lang",
+          description="Span reasoning in language.",
+      ),
+  ]
+
+  def _info(self):
+    return datasets.DatasetInfo(
+        description=(
+            "Simple Voice Queries (SVQ) dataset, Task: span reasoning in"
+            " language."
+        ),
+        features=datasets.Features({
+            "utt_id": datasets.Value("string"),
+            "waveform": datasets.Value("float32"),
+            "text": datasets.Value("string"),
+            "locale": datasets.Value("string"),
+            "environment": datasets.Value("string"),
+            "speaker_id": datasets.Value("string"),
+            "speaker_age": datasets.Value("int32"),
+            "speaker_gender": datasets.Value("string"),
+            "page_id": datasets.Value("string"),
+            "page_title": datasets.Value("string"),
+            "passage_id": datasets.Value("string"),
+            "passage_text": datasets.Value("string"),
+            "span": datasets.Value("string"),
+        }),
+        homepage="https://huggingface.co/datasets/google/svq",
+        license="Apache 2.0",
+        citation=_CITATION,
+    )
