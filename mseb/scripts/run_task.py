b41fce72cc3fab851468285e1dc193670bf6063a
Tom Bagby <tombagby@google.com>
2025-09-08 14:04:02 -0700

    Correct class constructor call in run_task
    PiperOrigin-RevId: 804578421

--- mseb/scripts/run_task.py
+++ mseb/scripts/run_task.py
@@ -74,7 +74,7 @@ def main(argv):
       num_threads=_NUM_THREADS.value,
   )
   task_cls: Type[task_lib.MSEBTask] = tasks.get_task_by_name(_TASK.value)
-  task = task_cls(cache_dir=_CACHE_DIR.value)
+  task = task_cls()
   task.setup()
   results = leaderboard.run_benchmark(
       encoder_name=encoder_name, runner=runner, task=task
