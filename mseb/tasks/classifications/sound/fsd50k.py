b34475b8ad7f8a3f16c89918291ffc7998bb4c66
Ehsan Variani <variani@google.com>
2025-10-20 15:36:51 -0700

    Add fsd50k sound classification task.
    PiperOrigin-RevId: 821833561

---
+++ mseb/tasks/classifications/sound/fsd50k.py
@@ -0,0 +1,102 @@
+# Copyright 2025 The MSEB Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""FSD50K multi-label classification tasks."""
+
+from typing import Iterable, Sequence
+
+from mseb import types
+from mseb.datasets import fsd50k
+from mseb.evaluators import classification_evaluator
+from mseb.tasks import classification
+
+
+ReferenceType = classification_evaluator.MultiLabelClassificationReference
+
+
+class FSD50KClassification(classification.ClassificationTask):
+  """Base class for multi-label classification on the FSD50K dataset."""
+
+  split: str | None = None
+
+  @property
+  def task_type(self) -> str:
+    return "multi_label"
+
+  @property
+  def sub_tasks(self) -> list[str]:
+    return ["classification"]
+
+  def class_labels(self) -> Sequence[str]:
+    # The class labels are the same regardless of the split.
+    fsd_dataset = fsd50k.FSD50KDataset(split="test")
+    return fsd_dataset.class_labels
+
+  def sounds(self) -> Iterable[types.Sound]:
+    fsd_dataset = fsd50k.FSD50KDataset(split=self.split)
+    for record in fsd_dataset.get_task_data().to_dict("records"):
+      yield fsd_dataset.get_sound(record)
+
+  def examples(self, sub_task: str) -> Iterable[ReferenceType]:
+    if self.split is None:
+      raise ValueError("`split` must be set by a concrete task subclass.")
+
+    fsd_dataset = fsd50k.FSD50KDataset(split=self.split)
+    for _, record in enumerate(fsd_dataset.get_task_data().to_dict("records")):
+      example_id = str(record["fname"])
+      label_ids = record["labels"].split(",")
+      yield classification_evaluator.MultiLabelClassificationReference(
+          example_id=example_id,
+          label_ids=label_ids,
+      )
+
+
+class FSD50KTestClassification(FSD50KClassification):
+  split = "test"
+
+  metadata = types.TaskMetadata(
+      name="FSD50KTestClassification",
+      description=(
+          "Multi-label sound event classification on the test split of "
+          "the FSD50K dataset."
+      ),
+      reference="""@article{fonseca2022fsd50k,
+ author  = {Eduardo Fonseca and Xavier Favory and Jordi Pons and Frederic Font and Xavier Serra},
+ title   = {{FSD50K}: an Open Dataset of Human-Labeled Sound Events},
+ journal   = {IEEE/ACM Transactions on Audio, Speech, and Language Processing},
+ volume  = {30},
+ pages   = {829--852},
+ year    = {2021},
+ doi     = {10.1109/TASLP.2022.3149014}
+}""",
+      type="Classification",
+      category="audio",
+      main_score="mAP",
+      revision="1.0.0",
+      dataset=types.Dataset(
+          path="https://huggingface.co/datasets/Fhrozen/FSD50k",
+          revision="1.0.0",
+      ),
+      scores=[
+          classification_evaluator.mean_average_precision(),
+          classification_evaluator.micro_f1(),
+          classification_evaluator.macro_f1(),
+          classification_evaluator.hamming_loss(),
+          classification_evaluator.subset_accuracy(),
+      ],
+      eval_splits=["test"],
+      eval_langs=["und"],
+      domains=["audio", "acoustic", "environmental"],
+      task_subtypes=["classification"],
+  )
---
+++ mseb/tasks/classifications/sound/fsd50k_test.py
@@ -0,0 +1,102 @@
+# Copyright 2025 The MSEB Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from unittest import mock
+
+from absl.testing import absltest
+from absl.testing import flagsaver
+from mseb import dataset
+from mseb.tasks.classifications.sound import fsd50k
+import numpy as np
+import pandas as pd
+from scipy.io import wavfile
+
+
+class FSD50KTestClassificationTest(absltest.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.testdata_dir = self.create_tempdir()
+    labels_dir = os.path.join(self.testdata_dir.full_path, 'labels')
+    os.makedirs(labels_dir)
+
+    vocab_data = pd.DataFrame({
+        'index': [0, 1, 2], 'mid': ['m0', 'm1', 'm2'],
+        'display_name': ['Bark', 'Meow', 'Siren'],
+    })
+    vocab_data.to_csv(
+        os.path.join(labels_dir, 'vocabulary.csv'),
+        index=False,
+        header=False
+    )
+
+    # This mock data ONLY contains 'test' records.
+    eval_data = pd.DataFrame({
+        'fname': [1234, 5678, 121314],
+        'labels': ['Bark', 'Meow', 'Bark,Siren'],
+        'split': ['test', 'test', 'test'],
+    })
+    eval_data.to_csv(os.path.join(labels_dir, 'eval.csv'), index=False)
+
+    clips_dir = os.path.join(self.testdata_dir.full_path, 'clips', 'eval')
+    os.makedirs(clips_dir)
+    wavfile.write(
+        os.path.join(clips_dir, '1234.wav'),
+        16000,
+        np.zeros(16000, dtype=np.int16)
+    )
+    wavfile.write(
+        os.path.join(clips_dir, '5678.wav'),
+        16000,
+        np.zeros(24000, dtype=np.int16)
+    )
+    wavfile.write(
+        os.path.join(clips_dir, '121314.wav'),
+        16000,
+        np.zeros(32000, dtype=np.int16)
+    )
+
+    self.enter_context(
+        flagsaver.flagsaver(
+            (dataset._DATASET_BASEPATH, self.testdata_dir.full_path)
+        )
+    )
+    self.enter_context(
+        mock.patch('mseb.utils.download_from_hf', return_value=None)
+    )
+
+  def test_sounds(self):
+    task = fsd50k.FSD50KTestClassification()
+    sounds = list(task.sounds())
+    self.assertLen(sounds, 3)
+    self.assertEqual(sounds[0].context.id, '1234')
+    self.assertEqual(sounds[1].context.id, '5678')
+    self.assertEqual(sounds[2].context.id, '121314')
+
+  def test_examples(self):
+    task = fsd50k.FSD50KTestClassification()
+    examples = list(task.examples('classification'))
+    self.assertLen(examples, 3)
+    self.assertEqual(examples[0].example_id, '1234')
+    self.assertListEqual(examples[0].label_ids, ['Bark'])
+
+  def test_class_labels(self):
+    task = fsd50k.FSD50KTestClassification()
+    labels = task.class_labels()
+    self.assertListEqual(list(labels), ['Bark', 'Meow', 'Siren'])
+
+
+if __name__ == '__main__':
+  absltest.main()
