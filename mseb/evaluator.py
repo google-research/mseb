Bug fix for WER computation with empty transcript truth.
PiperOrigin-RevId: 919064223

mseb/evaluator.py
@@ -125,21 +125,20 @@ class SoundEmbeddingEvaluator(abc.ABC):
 def compute_weighted_average_and_std(
     values: list[types.WeightedValue],
 ) -> tuple[float, float]:
-  """Computes weighted average and standard deviation for a list of values."""
+  """Computes weighted mean and standard deviation for a list of values."""
 
   weights = np.array([x.weight for x in values])
-  mean = np.average(
-      np.array([x.value for x in values]),
-      weights=weights,
-  )
+  values = np.array([x.value for x in values])
+  weight = np.sum(weights)
+  mean = np.sum(values) / weight
   std = (
       np.average(
-          (np.array([x.value for x in values]) - mean) ** 2,
+          (np.where(weights == 0, values, values / weights) - mean) ** 2,
           weights=weights,
       )
       ** 0.5
   )
-  return mean, std
+  return float(mean), float(std)
 
 
 DistanceFn = Callable[

mseb/evaluator_test.py
@@ -122,12 +122,23 @@ class EvaluatorTest(absltest.TestCase):
     mean, std = evaluator.compute_weighted_average_and_std(
         values=[
             types.WeightedValue(value=3.0),
-            types.WeightedValue(value=2.0, weight=2.0),
-            types.WeightedValue(value=1.0, weight=3.0),
+            types.WeightedValue(value=2.0 * 2.0, weight=2.0),
+            types.WeightedValue(value=1.0 * 3.0, weight=3.0),
         ],
     )
     npt.assert_almost_equal(mean, 5 / 3)
     npt.assert_almost_equal(std**2, 5 / 9)
+
+  def test_compute_weighted_average_and_std_with_weights_with_zero_weight(self):
+    mean, std = evaluator.compute_weighted_average_and_std(
+        values=[
+            types.WeightedValue(value=3.0),
+            types.WeightedValue(value=2.0 * 2.0, weight=2.0),
+            types.WeightedValue(value=3.0, weight=0.0),
+        ],
+    )
+    npt.assert_almost_equal(mean, 10 / 3)
+    npt.assert_almost_equal(std**2, 11 / 9)
 
   def test_top_1(self):
     scores = np.array([2, 8, 5, 0])